d5043471c1e3781aca60eb241921987e17d4f012
MohammedRaji <rajimohammed1992@gmail.com>
2020-07-21 01:46:59 +0100

    Updates

---
+++ precache-manifest.bfb82581635e23a209457baedccc9191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "010c86c188b1b325660b",
+    "url": "/static/css/main.9925785b.chunk.css"
+  },
+  {
+    "revision": "010c86c188b1b325660b",
+    "url": "/static/js/main.aa3f47a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37f6947511a5dd1660a4",
+    "url": "/static/js/2.3f241aa5.chunk.js"
+  },
+  {
+    "revision": "c91c7ca7dc6a92e44de4433e27cd4626",
+    "url": "/static/media/logo.c91c7ca7.svg"
+  },
+  {
+    "revision": "bf003d93aa9a58befcde578b39683715",
+    "url": "/index.html"
+  }
+];
